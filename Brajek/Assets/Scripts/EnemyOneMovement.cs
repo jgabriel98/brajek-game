df0d14e8896e1eae11f8cdfdced14661128ca430
Leandro Vianna <leandro97vianna@gmail.com>
2019-12-10 14:49:15 -0300

    add enemy that only follow the player

---
+++ Brajek/Assets/Scripts/EnemyOneMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyOneMovement : MonoBehaviour
+{
+    public float speed = 2f;
+    public float range = 5f;
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      float distance = Vector3.Distance(transform.position, 
+          target.transform.position);
+
+      if (distance < range) {
+        follow(target.transform);
+      }
+    }
+
+    private void follow(Transform target) {
+      Vector3 dir = target.transform.position - transform.position;
+      dir.Normalize();
+
+      transform.position += dir * speed * Time.deltaTime;
+    }
+}
